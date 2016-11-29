c205445d059e2aa6394ec322286715adc74a8835
sajibhasan <sajib001@gmail.com>
2016-11-29 12:35:35 +0600

    create and view

---
+++ resources/views/manageChild.blade.php
@@ -0,0 +1,19 @@
+<ul>
+
+@foreach($childs as $child)
+
+	<li>
+
+	    {{ $child->title }}
+
+	@if(count($child->childs))
+
+            @include('manageChild',['childs' => $child->childs])
+
+        @endif
+
+	</li>
+
+@endforeach
+
+</ul>
